004ebd2fd6a8afbdadb267a919d921062ffa93ff
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-05-06 15:21:44 +0800

    Update miner.h

--- src/miner.h
+++ src/miner.h
@@ -2,22 +2,23 @@
 # ifdef MINER_RUN_H
 # define MINER_RUN_H
 
+#include <functional>
+#include <algorithm>
+
 namespace map_evaluator {
 	template<typename ArrType>
-	class base
+	std::function<int(int, int)> simple(const ArrType **mp, int row, int col, int k)
 	{
-	protected:
-		int map_row, map_col;
-		const ArrType **space;
-		base(const ArrType **space, int row, int col)
-		{
-			
+		int dp[k + 1][k + 1];
+		return [mp, row, col, k] (int x, int y) {
+			int tc;
+			for (int i = min(x + k - 1, row - 2); i >= x; i--) {
+				for (int j = min(y + k - (i - x - k + 2), col - 2); j >= y; j--) {
+					dp[i][j] = 
+				}
+			}
+			return 
 		}
-	}
-
-	class simple
-	{
-
 	}
 }
 
